a7b6f92579801a9201e41c6bfb92687865bbf9e1
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-03-14 16:59:48 +0000

    add newGame, addTurn amd showScore functions

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,8 @@
 * @jest-environment jsdom
 */
 
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
+
 
 beforeAll(() => {
 	let fs = require("fs");
@@ -25,4 +26,29 @@ describe("game object contains correct keys", () => {
   	test("choices key exists", () => {
       	expect("choices" in game).toBe(true);
     });
-})
+	test("choices contain correct ID's",() => {
+		expect(game.choices).toEqual(["button1", "button2", "button3", "button4"])
+	});
+});
+
+describe("newGame works correctly", () => {
+	beforeAll(() => {
+		game.score = 42;
+		game.playerMoves = ["button1", "button2"];
+		game.currentGame = ["button1", "button2"];
+		document.getElementById("score").innerText = "42"
+		newGame();
+	});
+	test("should set game score to zero", () => {
+		expect(game.score).toEqual(0);
+	});
+	test("should be one element in the computers array", () => {
+    	expect(game.currentGame.length).toBe(1);
+  	});
+	test("should clear the player moves array", () => {
+    	expect(game.playerMoves.length).toBe(0);
+  	});
+	test("should display 0 for the element with Id of score", () => {
+    	expect(document.getElementById("score").innerText).toEqual(0);
+  	});
+});
